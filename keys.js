1d201b426076da8634c8a0cb797ad7c90116ba2d
ingan <isabelngan27@gmail.com>
2019-08-12 18:01:14 -0700

    keys

--- keys.js
+++ keys.js
@@ -1,1 +1,17 @@
+//get your API creds by following these steps:
+//Step One: https://apps.twitter.com/app/new
+//Step Two: use http:// for your urls
+//Step Three: then go to Keys and Access Tokens to get your credentials for below
+//Step Four: then you have to click the button below on that page to create an access token
+exports.omdb = {
+    key: process.env.OMDB_KEY,
+};
 
+exports.spotify = {
+    id: process.env.SPOTIFY_CLIENT_ID,
+    secret: process.env.SPOTIFY_CLIENT_SECERET
+};
+
+exports.bandsInTown = {
+    id: process.env.BANDSINTOWN_ID,
+};
